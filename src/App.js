c3ce0f15ab4e0ae9d454162eb0556805e2f67831
Abdul Manan <abdulmanankam@gmail.com>
2023-06-07 12:48:26 +0500

    first commit
    CRUD operation with ecommerce products in react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import Product from './pages/Frontend/Product';
+
+function App() {
+  return (
+    <>
+      <Product />
+    </>
+  );
+}
+
+export default App;
